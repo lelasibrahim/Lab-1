d023ee9ac3895b41800c339f5c67fef74429e642
lelasibrahim <2270002291@iau.edu.sa>
2026-09-11 17:10:42 +0300

    Add files via upload
    Task 1 C++

---
+++ Task 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v1,v2,product;
+    cout<<"Enter the first number: ";
+    cin>>v1;
+    cout<<"Enter the second number: ";
+    cin>>v2;
+    product=v1*v2;
+    cout<<"The product is :"<<product;
+
+    return 0;
+}
